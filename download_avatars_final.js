94e43e4c9dcc4c60b895858daa82329fd39e5289
tofurocks <mattwall0@gmail.com>
2018-09-03 19:34:00 -0700

    provide command line arguments to download collaborator images

---
+++ download_avatars_final.js
@@ -0,0 +1,54 @@
+var request = require('request');
+var fs = require('fs');
+var myArgs = process.argv.slice(2)
+var repOwner = myArgs[0]
+var repName = myArgs[1]
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+if(myArgs.length < 2){
+    throw new error("You didn't provide enough aguments, dummy!")
+}
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token <removed>'
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, JSON.parse(body));
+    });
+  }
+
+  getRepoContributors(repOwner, repName, function(err, result) {
+      if(err){
+        console.log("Errors:", err);
+      }
+      else {
+    for(contributor in result){
+        if(contributor){
+        //console.log(result[contributor].avatar_url)
+        var imgLoc = result[contributor].avatar_url
+        console.log("img is at " + imgLoc)
+        downloadImageByURL(imgLoc, contributor + ".jpg")
+        }
+    }
+    }
+  });
+
+  function downloadImageByURL(url, filePath) {
+
+    request.get(url)  
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+  }
+
+  //downloadImageByURL("https://avatars1.githubusercontent.com/u/43004?v=4", "test33.jpg")
