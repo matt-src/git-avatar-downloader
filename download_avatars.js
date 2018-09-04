beb1af0ceb4ba986af39faef3dd296493dfe72e2
tofurocks <mattwall0@gmail.com>
2018-09-03 18:52:27 -0700

    now saves all contributors' images to files

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,7 +8,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
         'User-Agent': 'request',
-        'Authorization': 'token <token here (removed)>'
+        'Authorization': 'token <removed>'
       }
     };
   
@@ -23,8 +24,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
       else {
     for(contributor in result){
         if(contributor){
-        console.log(result[contributor].avatar_url)
+        //console.log(result[contributor].avatar_url)
+        var imgLoc = result[contributor].avatar_url
+        console.log("img is at " + imgLoc)
+        downloadImageByURL(imgLoc, contributor + ".jpg")
         }
     }
     }
   });
+
+  function downloadImageByURL(url, filePath) {
+
+    request.get(url)  
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+  }
+
+  downloadImageByURL("https://avatars1.githubusercontent.com/u/43004?v=4", "test33.jpg")
