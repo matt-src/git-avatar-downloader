8b9c266d9c44b8da21aa9ddbcc3969ea52923a5f
tofurocks <mattwall0@gmail.com>
2018-09-03 19:57:38 -0700

    added comments to code

--- download_avatars_final.js
+++ download_avatars_final.js
@@ -1,15 +1,19 @@
 var request = require('request');
 var fs = require('fs');
-var myArgs = process.argv.slice(2)
+//Take arguments from command line and assign them to repOwner and repName
+var myArgs = process.argv.slice(2) 
 var repOwner = myArgs[0]
 var repName = myArgs[1]
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+//Make sure we have at least 2 arguments, otherwise throw error
 if(myArgs.length < 2){
     throw new error("You didn't provide enough aguments, dummy!")
 }
 
+//Get the list of repo contributors, parse it to an object, and then pass it to the callback function
 function getRepoContributors(repoOwner, repoName, cb) {
+    //Set the options for our request 
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
@@ -17,28 +21,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
         'Authorization': 'token <removed>'
       }
     };
-  
+    //Make our request, and then pass the body we get back to our callback function
     request(options, function(err, res, body) {
       cb(err, JSON.parse(body));
     });
   }
 
+  //Call getRepoContributors, pass in our repo name and repo owner that we took as arguments, plus our callback to extract avatar url's
   getRepoContributors(repOwner, repName, function(err, result) {
       if(err){
         console.log("Errors:", err);
       }
       else {
+          //Extract avatar url's from contributors and pass them to downloadImageByURL
     for(contributor in result){
         if(contributor){
         //console.log(result[contributor].avatar_url)
         var imgLoc = result[contributor].avatar_url
-        console.log("img is at " + imgLoc)
+        //Download image and save it at <contributor number>.jpg
         downloadImageByURL(imgLoc, contributor + ".jpg")
         }
     }
     }
   });
 
+  //Downloads an image from the given url and saves it at the given filePath
   function downloadImageByURL(url, filePath) {
 
     request.get(url)  
@@ -48,7 +55,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
        .on('response', function (response) {
          console.log('Response Status Code: ', response.statusCode);
        })
-       .pipe(fs.createWriteStream(filePath));
+       .pipe(fs.createWriteStream(filePath)); //We successfully got the image, so save it to <filePath>
   }
 
   //downloadImageByURL("https://avatars1.githubusercontent.com/u/43004?v=4", "test33.jpg")
